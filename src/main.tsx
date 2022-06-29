1f997be9619d045990adb8554cb8ead0da8e9117
samy senani <aceportgasd235@gmail.com>
2022-06-29 12:44:08 +0100

    add: eslint / prettier config add: readme file add: apollo client setup --> project

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,25 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import App from "./App";
 import "./index.css";
 
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 
+const client = new ApolloClient({
+  uri: "http://localhost:5000",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <Suspense fallback={<p> Loading... </p>}>
-        <ColorModeScript />
-        <App />
-      </Suspense>
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <Suspense fallback={<p> Loading... </p>}>
+          <ColorModeScript />
+          <App />
+        </Suspense>
+      </ChakraProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
